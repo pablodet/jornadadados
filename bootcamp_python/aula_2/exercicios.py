f3e32adaa64787c6a6bdcb7b459ddf1cda8df0fa
Data Det <datadet2025@gmail.com>
2025-03-19 20:04:20 -0300

    Criando um programa que receba uma string do usuário e a converta para maiúsculas

--- bootcamp_python/aula_2/exercicios.py
+++ bootcamp_python/aula_2/exercicios.py
@@ -65,12 +65,14 @@
 # #### Strings (`str`)
 
 # 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
-
+texto = input("Digite um texto: ")
+texto_maiusculas = texto.upper()
+print("Texto em maiúsculas:", texto_maiusculas)
 
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
-nome_completo = input("Digite o nome completo: ")
-nome_minusculas = nome_completo.lower()
-print("Nome em minúsculas:", nome_minusculas)
+#nome_completo = input("Digite o nome completo: ")
+#nome_minusculas = nome_completo.lower()
+#print("Nome em minúsculas:", nome_minusculas)
 
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 ## 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
