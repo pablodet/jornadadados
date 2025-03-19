667783f69dda02314c821b9e52efb470c099b52d
Data Det <datadet2025@gmail.com>
2025-03-19 19:59:09 -0300

    Criando um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas

--- bootcamp_python/aula_2/exercicios.py
+++ bootcamp_python/aula_2/exercicios.py
@@ -68,13 +68,17 @@
 
 
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+nome_completo = input("Digite o nome completo: ")
+nome_minusculas = nome_completo.lower()
+print("Nome em minúsculas:", nome_minusculas)
+
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
-# 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
-data_do_usuario = input("Insira uma data no formato dd/mm/aaaa: ")
-lista_de_dia_mes_ano = data_do_usuario.split("/")
-print(f"O dia é: {lista_de_dia_mes_ano[0]}")
-print(f"O mês é: {lista_de_dia_mes_ano[1]}")
-print(f"O ano é: {lista_de_dia_mes_ano[2]}")
+## 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+#data_do_usuario = input("Insira uma data no formato dd/mm/aaaa: ")
+#lista_de_dia_mes_ano = data_do_usuario.split("/")
+#print(f"O dia é: {lista_de_dia_mes_ano[0]}")
+#print(f"O mês é: {lista_de_dia_mes_ano[1]}")
+#print(f"O ano é: {lista_de_dia_mes_ano[2]}")
 
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
 
