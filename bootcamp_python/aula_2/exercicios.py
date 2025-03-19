abdb3e7fbff981de466170785ea3a2b1901cadd1
Data Det <datadet2025@gmail.com>
2025-03-19 01:12:36 -0300

    Crie um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo

--- bootcamp_python/aula_2/exercicios.py
+++ bootcamp_python/aula_2/exercicios.py
@@ -1,14 +1,20 @@
 # #### Inteiros (`int`)
 
 # 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
-num1 = int(input("Digite o primeiro numero inteiro: "))
-num2 = int(input("Digite o segundo numero inteiro: "))
-resultado_soma = num1 + num2
-print("A soma é:", resultado_soma)
+#num1 = int(input("Digite o primeiro numero inteiro: "))
+#num2 = int(input("Digite o segundo numero inteiro: "))
+#resultado_soma = num1 + num2
+#print("A soma é:", resultado_soma)
 
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+num1 = int(input("Digite o primeiro numero inteiro: "))
+num2 = int(input("Digite o segundo numero inteiro: "))
+resultado_divisao_inteira = num1 / num2
+print("o resultado da divisão inteira é:", resultado_divisao_inteira)
+
+
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
 
 # #### Números de Ponto Flutuante (`float`)
